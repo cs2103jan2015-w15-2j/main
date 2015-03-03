f13c8b63785516b34ca65a0425fcbb11fe57ebbe
davis12529 <davis12529@hotmail.com>
2015-03-03 21:47:51 +0800

    Added IParser interface, Command.java and Parser.java skeletal code

--- src/com/nexus/simplify/Command.java
+++ src/com/nexus/simplify/Command.java
@@ -1,5 +1,30 @@
 package com.nexus.simplify;
 
 public class Command {
+	enum OperationType {
+		ADD, DELETE, MODIFY, ARCHIVE, INVALID
+	};
+	
+	OperationType operation;
+	
+	final String[] parameter;
 
+	/**
+	 * Constructor. Takes in a string array containing all parameters
+	 * 
+	 * @param parameter
+	 */
+	public Command(OperationType operation, String[] parameter) {
+		super();
+		this.parameter = parameter;
+	}
+
+	// Getters
+	public String[] getParamter() {
+		return parameter;
+	}
+	
+	
+	
+	
 }
--- src/com/nexus/simplify/Parser.java
+++ src/com/nexus/simplify/Parser.java
@@ -1,5 +1,38 @@
 package com.nexus.simplify;
 
-public class Parser {
+import com.nexus.simplify.Command.OperationType;
+
+public class Parser implements IParser {
+	
+	/*
+	 * http://stackoverflow.com/questions/717588/writing-a-cleaner-and-more-modular-command-parser
+	 * 
+	 */
+	
+	// Need identify Keywords in enum or hash, etc
+	
+	// List of all possible tokens
+	enum Token {
+		COMMAND, NAME, DUEDATE, WORKLOAD
+	}
+	
+	/**
+	 * Lexical analyser which takes in a string and produces a list of tokens
+	 * @param userInput
+	 * @return 
+	 */
+	private Token[] tokeniser(String userInput){
+		return null;
+	}
+	
+	private Command parseTokens(Token[] tokenList){
+		return new Command(OperationType.INVALID, null);
+	}
+	
+	public String parseInput(String userInput) {
+		Token[] userTokens = tokeniser(userInput);
+		Command userCommand = parseTokens(userTokens);		
+		return Logic.executeCommand(userCommand);
+	}
 
 }
