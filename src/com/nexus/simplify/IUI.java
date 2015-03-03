e06396914af3ed144e40c9a855ecf3e93a4c8b14
jftoh <tohjianfeng@gmail.com>
2015-03-03 19:56:44 +0800

    1. Added interface UI (IUI.java) 2. Added skeletal methods and constructor to UI.java

--- src/com/nexus/simplify/IUI.java
+++ src/com/nexus/simplify/IUI.java
@@ -1,5 +1,7 @@
 package com.nexus.simplify;
 
 public interface IUI {
-
+	public void run();
+	
+	public void displayFeedback();
 }
--- src/com/nexus/simplify/UI.java
+++ src/com/nexus/simplify/UI.java
@@ -1,5 +1,39 @@
 package com.nexus.simplify;
 
-public class UI {
+import java.util.Scanner;
 
+public class UI implements IUI {
+	
+	//-----------------//
+	// Class Variables //
+	//-----------------//
+	
+	Scanner scanner;
+	
+	private static final String PROMPT = "command: ";
+	
+	//-------------//
+	// Constructor //
+	//-------------//
+	
+	public UI() {
+		scanner = new Scanner(System.in);
+		run();
+	}
+	
+	//------------------------//
+	// Program Initialization //
+	//------------------------//
+	
+	@Override
+	public void run() {
+		
+	}
+	
+	
+	
+	@Override
+	public void displayFeedback() {
+		
+	}
 }
