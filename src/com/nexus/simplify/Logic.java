13563ba7d90a674e415e8d96e1a792e522124b37
David Zhao Han <davidzhaohan@gmail.com>
2015-03-10 15:52:01 +0800

    added subclasses for "Command" class

--- src/com/nexus/simplify/Logic.java
+++ src/com/nexus/simplify/Logic.java
@@ -4,12 +4,17 @@ import com.nexus.simplify.Parser.OperationType;
 
 public class Logic implements ILogic {
 	
+	private TaskList tempList;
+	
+	public Logic(){
+		tempList = new TaskList();
+	}
+	
 	public Result executeCommand(Command command){
 		OperationType operation = command.getOperation();
 		String[] parameter = command.getParameter();
 		return command.executeSpecificCommand(operation, parameter);
 	}
-	
 	
 	public TaskList initialise(){
 		Database database = new Database();
