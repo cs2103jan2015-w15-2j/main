d07a288e9234f7e6b0dc6726bb82208753061c7f
Jaelyyn <tan.qianyi12@gmail.com>
2015-03-10 20:19:10 +0800

    Added json external library and updated .gitignore file Added getDataFromFile and included duedate when converting between json and java for Database class Updated getDueDate method for Task class

--- src/com/nexus/simplify/Task.java
+++ src/com/nexus/simplify/Task.java
@@ -1,6 +1,8 @@
 package com.nexus.simplify;
 
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 public class Task {	
 	// instance variables
@@ -14,7 +16,7 @@ public class Task {
 		this.name = name;
 		this.workload = 1;
 		// is id going to be generated at logic or task?
-		this.dueDate = new DateTime(0, 0, 0, 0, 0);
+		this.dueDate = new DateTime(0, 1, 1, 0, 0);
 	}
 	
 	public Task(String name, int day, int month, int year, int hour, int minute, String workload, String id) {
@@ -27,15 +29,19 @@ public class Task {
 	// methods
 	public String getName() { return name; }
 	public void setName(String name) { this.name = name; }
-	public DateTime getDueDate() { return this.dueDate; }
 	public void setDueDate(int day, int month, int year, int hour, int minute) { this.dueDate = new DateTime(year, month, day, hour, minute); }
+	public void setDueDate(DateTime dueDate) { this.dueDate = dueDate; }
 	public int getWorkload() { return this.workload; }
 	public void setWorkload(String workload) { this.workload = Integer.valueOf(workload); }
 	public String getId() { return this.id; }
 	public void setId(String id) { this.id = id; }
 	
+	public String getDueDate() {
+		DateTimeFormatter format = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm");
+		return format.print(this.dueDate);
+	}
 	public boolean isFloatingTask() {
-		if (dueDate == new DateTime(0, 0, 0, 0, 0)) {
+		if (dueDate.getYear() == 0) {
 			return true;
 		} else {
 			return false;
