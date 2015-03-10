9b67940bf108fb59a2d46e8b7bb488940e0364e4
David Zhao Han <davidzhaohan@gmail.com>
2015-03-10 17:14:25 +0800

    take a break again

--- src/com/nexus/simplify/Add.java
+++ src/com/nexus/simplify/Add.java
@@ -1,5 +1,20 @@
 package com.nexus.simplify;
 
 public class Add {
+	private static final int NAME_POS = 0;
+	private static final int DEADLINE_POS = 1;
+	private static final int WORKLOAD_POS = 2;
 	
+	public Add(){}
+	
+	public CommandResult execute(String[] parameter){
+		String name = parameter[NAME_POS];
+		String deadline = parameter[DEADLINE_POS];
+		String workload = parameter[WORKLOAD_POS];
+		
+		
+		writeToFile();
+		return null;
+		
+	}
 }
