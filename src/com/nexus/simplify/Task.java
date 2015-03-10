3e69220cf0d7e6bdc601440126231a6d3d8cd2e6
Jaelyyn <tan.qianyi12@gmail.com>
2015-03-10 17:44:12 +0800

    Replaced DueDate with DateTime

--- src/com/nexus/simplify/Task.java
+++ src/com/nexus/simplify/Task.java
@@ -1,31 +1,44 @@
 package com.nexus.simplify;
 
+import org.joda.time.DateTime;
+
 public class Task {	
 	// instance variables
 	private String name;
-	private DueDate dueDate;
+	private DateTime dueDate;
 	int workload;
 	private String id;
 	
 	// constructors
 	public Task(String name) {
 		this.name = name;
+		this.workload = 1;
+		// is id going to be generated at logic or task?
+		this.dueDate = new DateTime(0, 0, 0, 0, 0);
 	}
 	
-	public Task(String name, String day, String month, String year, String workload, String id) {
+	public Task(String name, int day, int month, int year, int hour, int minute, String workload, String id) {
 		this.name = name;
 		this.workload = Integer.valueOf(workload);
 		this.id = id;
+		this.dueDate = new DateTime(year, month, day, hour, minute);
 	}
 	
 	// methods
-	public boolean isFloatingTask() { return !dueDate.hasDueDate(); }
 	public String getName() { return name; }
 	public void setName(String name) { this.name = name; }
-	public DueDate getDueDate() { return this.dueDate; }
-	public void setDueDate(String day, String month, String year) {	}
+	public DateTime getDueDate() { return this.dueDate; }
+	public void setDueDate(int day, int month, int year, int hour, int minute) { this.dueDate = new DateTime(year, month, day, hour, minute); }
 	public int getWorkload() { return this.workload; }
 	public void setWorkload(String workload) { this.workload = Integer.valueOf(workload); }
 	public String getId() { return this.id; }
 	public void setId(String id) { this.id = id; }
+	
+	public boolean isFloatingTask() {
+		if (dueDate == new DateTime(0, 0, 0, 0, 0)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
