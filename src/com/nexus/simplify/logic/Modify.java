01e5f22a18081f28e0d4fe53426cc3f587ddb958
David Zhao Han <davidzhaohan@gmail.com>
2015-03-24 17:51:26 +0800

    made changes to Modify class, ready to merge with master on remote repo

--- src/com/nexus/simplify/logic/Modify.java
+++ src/com/nexus/simplify/logic/Modify.java
@@ -11,15 +11,6 @@ import com.nexus.simplify.logic.usercommand.ParameterType;
  * and calls APIs provided by database to modify storage.
  */
 public class Modify {
-	private static final int INVALID_OPTION = 0;
-	private static final int NAME_ONLY = 1;
-	private static final int DEADLINE_ONLY = 2;
-	private static final int NAME_AND_DEADLINE = 3;
-	private static final int WORKLOAD_ONLY = 4;
-	private static final int NAME_AND_WORKLOAD = 5;
-	private static final int DEADLINE_AND_WORKLOAD = 6;
-	private static final int ALL_FIELDS = 7;
-		 
 	public Modify() {}
 	
 	public String execute(String[] parameter){
@@ -34,77 +25,37 @@ public class Modify {
 			return feedback;
 		}
 		
-		int indexToModify = Integer.parseInt(parameter[ParameterType.INDEX_POS]);
-		String newName = parameter[ParameterType.NEW_NAME_POS];
-		String newStartTime = parameter[ParameterType.NEW_STARTTIME_POS];
-		String newEndTime = parameter[ParameterType.NEW_ENDTIME_POS];
-		
-		int newWorkload = Integer.parseInt(parameter[ParameterType.NEW_WORKLOAD_POS]);
 		Database database = new Database();
-		String feedback;
-		
-		int combinationNumber = decideCombination(newName,newStartTime,newEndTime,newWorkload);
-		switch(combinationNumber){
-			case INVALID_OPTION:
-				feedback = "please specify a(some) field(s) to modify.";
-				return feedback;
-			case NAME_ONLY:
-				database.modifyName(indexToModify,newName);
-				feedback = "task name modified";
-				return feedback;
-			case DEADLINE_ONLY:
-				database.modifyDeadline(indexToModify,newStartTime,newEndTime);
-				feedback = "task deadline modified.";
-				return feedback;
-			case NAME_AND_DEADLINE:
-				database.modifyNameAndDeadline(indexToModify,newName,newDeadline);
-				feedback = "task name and deadline modified.";
-				return feedback;
-			case WORKLOAD_ONLY:
-				database.modifyWorkloadOnly(indexToModify,newWorkload);
-				feedback = "task workload modified to " + newWorkload + ".";
-				return feedback;
-			case NAME_AND_WORKLOAD:
-				database.modifyNameAndWorkload(indexToModify,newName,newWorkload);
-				feedback = "task name and workload modified.";
-				return feedback;
-			case DEADLINE_AND_WORKLOAD:
-				database.modifyDeadlineAndWorkload(indexToModify,newDeadline,newWorkload);
-				feedback = "task deadline and workload modified.";
-				return feedback;
-			case ALL_FIELDS:
-				database.modifyAllFields(indexToModify,newName,newDeadline,newWorkload);
-				feedback = "task modified.";
-				return feedback;
-			default:
-				return "unexpected error.";
+		int indexToModify = Integer.parseInt(parameter[ParameterType.INDEX_POS]);
+		String feedback = "task ";
+		String newName = parameter[ParameterType.NEW_NAME_POS];
+		if(newName != null){
+			database.modifyName(newName);
+			feedback += "name ";
 		}
-	}
-	
-	private int decideCombination(String name, String start, String end, String workload){
-		if(name == null && deadline == null && workload == null){
-			return INVALID_OPTION;
+		String newStartTime = parameter[ParameterType.NEW_STARTTIME_POS];
+		if(newStartTime != null){
+			Date startTime = df.parse(newStartTime);
+			database.modifyStartTime(startTime);
+			feedback += "and start time ";
 		}
-		else if(name != null && deadline == null && workload == null){
-			return NAME_ONLY;
+		String newEndTime = parameter[ParameterType.NEW_ENDTIME_POS];
+		if(newEndTime != null){
+			Date endTime = df.parse(newEndTime);
+			database.modifyStartTime(endTime);
+			feedback += "and end time ";
 		}
-		else if(name == null && deadline != null && workload == null){
-			return DEADLINE_ONLY;
+		if(parameter[ParameterType.NEW_WORKLOAD_POS] != null){
+			try{
+				int newWorkload = Integer.parseInt(parameter[ParameterType.NEW_WORKLOAD_POS]);
+			}catch(NumberFormatException e){
+				return "please enter a valid workload.";
+			}
+			int newWorkload = Integer.parseInt(parameter[ParameterType.NEW_WORKLOAD_POS]);
+			database.modifyWorkload(newWorkload);
+			feedback += "and workload ";
 		}
-		else if(name != null && deadline != null && workload == null){
-			return NAME_AND_DEADLINE;
-		}
-		else if(name == null && deadline == null && workload != null){
-			return WORKLOAD_ONLY;
-		}
-		else if(name != null && deadline == null && workload != null){
-			return NAME_AND_WORKLOAD;
-		}
-		else if(name == null && deadline != null && workload != null){
-			return DEADLINE_AND_WORKLOAD;
-		}
-		else{
-			return ALL_FIELDS;
-		}
+		feedback += "modified.";
+		return feedback;
 	}
 }
--- src/com/nexus/simplify/logic/TestAdd.java
+++ src/com/nexus/simplify/logic/TestAdd.java
@@ -1,5 +1,5 @@
 package com.nexus.simplify.logic;
-/* commented out to facilitate tut8
+
 
 package com.nexus.simplify;
 
