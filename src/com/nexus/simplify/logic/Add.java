e19191d8456c8a43180bdec8edde038f99bc8130
David Zhao Han <davidzhaohan@gmail.com>
2015-03-24 22:49:25 +0800

    1. Added test cases for add operation 2. resolved merge conflict in Add.java

--- src/com/nexus/simplify/logic/Add.java
+++ src/com/nexus/simplify/logic/Add.java
@@ -20,29 +20,6 @@ public class Add {
 		String feedback;
 		Database database = new Database();
 		
-<<<<<<< HEAD
-		if(deadline == null){
-			database.addGenericTask(name,workload);
-			feedback = "successfully added floating task \"" + name + "\".";
-		}
-		else{
-			String[] splitResult = deadline.split(" ");
-			if(splitResult.length == 2){
-				String date = splitResult[0];
-				String time = splitResult[1];
-				String[] splitResult1 = date.split("/");
-				String[] splitResult2 = time.split(":");
-				int year = Integer.parseInt(splitResult1[0]);
-				int month = Integer.parseInt(splitResult1[1]);
-				int day = Integer.parseInt(splitResult1[2]);
-				int hour = Integer.parseInt(splitResult2[0]);
-				int minute = Integer.parseInt(splitResult2[1]);
-				database.addTimed(name,year,month,day,hour,minute,workload);
-				feedback = "successfully added timed task \"" + name + "\".";
-			}
-			else{
-				database.addDeadlineTask(name,deadline,workload);
-=======
 		if(newStartTime == null && newEndTime == null){
 			database.addFloating(name,workload);
 			feedback = "successfully added floating task \"" + name + "\".";
@@ -51,7 +28,6 @@ public class Add {
 			if(newStartTime.equals(newEndTime)){
 				Date deadline = df.parse(newStartTime);
 				database.addDeadline(name,deadline,workload);
->>>>>>> 01e5f22a18081f28e0d4fe53426cc3f587ddb958
 				feedback = "successfully added dealine task \"" + name + "\".";
 			}
 			else{
