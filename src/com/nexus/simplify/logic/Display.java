253253e4e85acd561c892764db6a313fbee99b3d
David Zhao Han <davidzhaohan@gmail.com>
2015-03-24 13:02:09 +0800

    modified Display class

--- src/com/nexus/simplify/logic/Display.java
+++ src/com/nexus/simplify/logic/Display.java
@@ -11,10 +11,39 @@ public class Display {
 		
 	public String execute(String[] parameter){
 		String option = parameter[0];
+		String feedback;
 		Database database = new Database();
 		if(isNumeric(option)){
 			database.display(option);
-			
+			feedback = "displayed " + option + "tasks.";
+			return feedback;
+		}
+		else{
+			switch(option){
+				case "all":
+					database.display(option);
+					feedback = "displayed all tasks.";
+					return feedback;
+				case "week":
+					database.display(option);
+					feedback = "displayed tasks due within a week.";
+					return feedback;
+				case "deadline":
+					database.display(option);
+					feedback = "displayed tasks by deadline";
+					return feedback;
+				case "workload":
+					database.display(option);
+					feedback = "displayed tasks by workload";
+					return feedback;
+				case null:
+					database.display("default");
+					feedback = "displayed tasks by default setting";
+					return feedback;
+				default:
+					feedback = "invalid option for display, please try again.";
+					return feedback;
+			}
 		}
 	}
 	
