baef02d611407d39cd1c2e68ea44d7ff6558302a
David Zhao Han <davidzhaohan@gmail.com>
2015-03-25 15:28:21 +0800

    some minor changes in comments for unit test class TestAdd.

--- src/com/nexus/simplify/logic/TestAdd.java
+++ src/com/nexus/simplify/logic/TestAdd.java
@@ -12,17 +12,17 @@ public class TestAdd {
 		String[] testParameter;
 		String feedback;
 		
-		// test case for floating tasks
+		// boundary test case for floating tasks partition
 		testParameter = new String[]{null, "play dota", null, null, "1"};
 		feedback = stubAdd.execute(testParameter);
 		assertEquals(feedback,"successfully added floating task \"play dota\".");
 		
-		// test case for timed tasks
+		// boundary test case for timed tasks partition
 		testParameter = new String[]{null, "anime seminar", "Jan 18 11:11", "Jan 18 12:22", "2"};
 		feedback = stubAdd.execute(testParameter);
 		assertEquals(feedback,"successfully added timed task \"anime seminar\".");
 		
-		// test case for deadline tasks
+		// boundary test case for deadline tasks partition
 		testParameter = new String[]{null, "finish game XYZ", "Tue Feb 22 10:00", "Tue Feb 22 10:00", "3"};
 		feedback = stubAdd.execute(testParameter);
 		assertEquals(feedback,"successfully added deadline task \"finish game XYZ\".");
