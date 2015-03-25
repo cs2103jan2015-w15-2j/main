58eeea18f23a43ab56638187463acc912362ec60
Toh Jian Feng <tohjianfeng@gmail.com>
2015-03-25 15:31:55 +0800

    add skeleton for testing

---
+++ src/com/nexus/simplify/database/DatabaseTest.java
@@ -0,0 +1,66 @@
+package com.nexus.simplify.database;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * provides a test suite for the various methods found in the Database Class.
+ * @author tohjianfeng
+ * */
+public class DatabaseTest {
+	
+	//------------------//
+	// Class Attributes //
+	//------------------//
+	
+	Database database;
+	
+	private static String fileName;
+	
+	private GenericTaskList genericTL;
+	private DeadlineTaskList deadlineTL;
+	private TimedTaskList timedTL;
+	
+	
+	//------------------------//
+	// Initialization Testing //
+	//------------------------//
+	
+	@BeforeClass
+	public void setUpBeforeClass() throws Exception {
+		
+	}
+	
+	@AfterClass
+	public void tearDownAfterClass() throws Exception {
+		
+	}
+	
+	//----------------//
+	// Method Testing //
+	//----------------//
+	
+	@Test
+	public void addTaskTest() {
+		
+	}
+	
+	@Test
+	public void deleteTaskTest() {
+		
+	}
+	
+	@Test
+	public void modifyTaskTest() {
+		
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
