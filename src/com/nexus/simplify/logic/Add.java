ad93266dd6ced538b93adb3732c1e93367986fa9
David Zhao Han <davidzhaohan@gmail.com>
2015-04-09 21:46:07 +0800

    modified feedbacks for add command

--- src/com/nexus/simplify/logic/Add.java
+++ src/com/nexus/simplify/logic/Add.java
@@ -18,9 +18,6 @@ public class Add {
 	private final String DATE_FORMAT_PATTERN = "E MMM dd HH:mm:ss zzz yyy";
 	private final String INVALID_WORKLOAD = "Please enter a valid workload.";
 	private final String NO_NAME = "Please enter a name for this task.";
-	private final String FLOATING_TASK_MESSAGE = "Floating task added at index ";
-	private final String DEADLINE_TASK_MESSAGE = "Deadline task added at index ";
-	private final String TIMED_TASK_MESSAGE = "Timed task added at index ";
 	
 	public Add() {}
 	
@@ -35,6 +32,7 @@ public class Add {
 		int workload;
 		Database database = MainApp.getDatabase();
 		
+		final String MESSAGE_TASK_ADDED = "Task \"" + name + "\" added successfully.";
 		// this if-else statement caters to workload
 		if (workloadStr == null || workloadStr.isEmpty()) {
 			workload = 0;
@@ -55,17 +53,17 @@ public class Add {
 		if((newStartTime == null || newStartTime.isEmpty()) &&
 		   (newEndTime == null || newEndTime.isEmpty())) {
 			database.addGenericTask(name, workload);
-			feedback = FLOATING_TASK_MESSAGE;
+			feedback =  MESSAGE_TASK_ADDED;
 		} else {
 			if(newStartTime.equals(newEndTime)) {
 				Date deadline = df.parse(newStartTime);
 				database.addDeadlineTask(name,deadline,workload);
-				feedback = DEADLINE_TASK_MESSAGE;
+				feedback = MESSAGE_TASK_ADDED;
 			} else {
 				Date startTime = df.parse(newStartTime);
 				Date endTime = df.parse(newEndTime);
 				database.addTimedTask(name,startTime,endTime,workload);
-				feedback = TIMED_TASK_MESSAGE;
+				feedback = MESSAGE_TASK_ADDED;
 			}
 		}
 		return feedback;
