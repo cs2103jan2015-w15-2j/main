73908db144fc649314d9771eadff44506a74885f
Jaelyyn <tan.qianyi12@gmail.com>
2015-04-12 00:01:34 +0800

    1. Refactored classes in Database package 2. Separated API of Database (DatabaseConnector) from actual Database class 3. Added interface for DatabaseConnector

--- src/com/nexus/simplify/database/Writer.java
+++ src/com/nexus/simplify/database/Writer.java
@@ -13,8 +13,12 @@ import com.nexus.simplify.database.tasktype.DeadlineTask;
 import com.nexus.simplify.database.tasktype.GenericTask;
 import com.nexus.simplify.database.tasktype.TimedTask;
 
+/**
+ * Represents an instance of a writer used to write to developer-specified file in JSON format.
+ * */
+
 public class Writer {
-	
+
 	private static final String TASK_TYPE_TIMED = "Timed";
 	private static final String TASK_TYPE_DEADLINE = "Deadline";
 	private static final String TASK_TYPE_GENERIC = "Generic";
@@ -29,148 +33,169 @@ public class Writer {
 	private static final String JSON_KEY_START_TIME = "Start Time";
 	private static final String JSON_KEY_NAME = "Name";
 	private static final String JSON_KEY_TYPE = "Type";
-	
-	Database database;
+
+	private Database database;
+
+	//-------------//
+	// Constructor //
+	//-------------//
 	
 	public Writer(Database database) {
 		this.database = database;
 	}
+	
 	//--------------//
 	// File Writing //
 	//--------------//
-	
-	public void writeToFile(ObservableList<GenericTask> inputObservableGeneric, ObservableList<DeadlineTask> inputObservableDeadline, ObservableList<TimedTask> inputObservableTimed, ObservableList<GenericTask> inputArchivedGeneric, ObservableList<DeadlineTask> inputArchivedDeadline, ObservableList<TimedTask> inputArchivedTimed) {
+
+	public void writeToFile(ObservableList<GenericTask> inputObservableGeneric,
+			ObservableList<DeadlineTask> inputObservableDeadline, ObservableList<TimedTask> inputObservableTimed,
+			ObservableList<GenericTask> inputArchivedGeneric, ObservableList<DeadlineTask> inputArchivedDeadline,
+			ObservableList<TimedTask> inputArchivedTimed) {
+		
 		try {
 			String fileName = database.getDataFilePath();
 			File outputFile = new File(fileName);
 			FileWriter fileWriter = new FileWriter(outputFile);
 			JSONArray jsonArrayForStorage = new JSONArray();
-			
+
+			convertGenericToStore(inputObservableGeneric, jsonArrayForStorage);
 			convertDeadlineToStore(inputObservableDeadline, jsonArrayForStorage);
 			convertTimedToStore(inputObservableTimed, jsonArrayForStorage);
-			convertGenericToStore(inputObservableGeneric, jsonArrayForStorage);
+			convertArchivedGenericToStore(inputArchivedGeneric, jsonArrayForStorage);
 			convertArchivedDeadlineToStore(inputArchivedDeadline, jsonArrayForStorage);
 			convertArchivedTimedToStore(inputArchivedTimed, jsonArrayForStorage);
-			convertArchivedGenericToStore(inputArchivedGeneric, jsonArrayForStorage);
-			
+
 			fileWriter.write(jsonArrayForStorage.toJSONString());
 			fileWriter.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} 
+		
 	}
-	
+
 	//---------------------//
 	// Variable Conversion //
 	//---------------------//
-		
+
 	@SuppressWarnings("unchecked")
 	private void convertGenericToStore(ObservableList<GenericTask> taskList, JSONArray jsonArrayForStorage) {
+		
 		if (!taskList.isEmpty()) {
 			for (int i = 0; i < taskList.size(); i++) {
-				GenericTask currGenericTask = taskList.get(i);
+				GenericTask currentGenericTask = taskList.get(i);
 				JSONObject jsonTask = new JSONObject();
-				
-				jsonTask.put(JSON_KEY_NAME, currGenericTask.getName());
-				jsonTask.put(JSON_KEY_WORKLOAD, currGenericTask.getWorkload());
-				jsonTask.put(JSON_KEY_ID, currGenericTask.getId());
+
+				jsonTask.put(JSON_KEY_NAME, currentGenericTask.getName());
+				jsonTask.put(JSON_KEY_WORKLOAD, currentGenericTask.getWorkload());
+				jsonTask.put(JSON_KEY_ID, currentGenericTask.getId());
 				jsonTask.put(JSON_KEY_TYPE, TASK_TYPE_GENERIC);
-				
+
 				jsonArrayForStorage.add(jsonTask);
 			}
 		}
+		
 	}
-	
-	@SuppressWarnings("unchecked")
-	private void convertArchivedGenericToStore(ObservableList<GenericTask> taskList, JSONArray jsonArrayForStorage) {
-		if (!taskList.isEmpty()) {
-			for (int i = 0; i < taskList.size(); i++) {
-				GenericTask currGenericTask = taskList.get(i);
-				JSONObject jsonTask = new JSONObject();
-				
-				jsonTask.put(JSON_KEY_NAME, currGenericTask.getName());
-				jsonTask.put(JSON_KEY_WORKLOAD, currGenericTask.getWorkload());
-				jsonTask.put(JSON_KEY_ID, currGenericTask.getId());
-				jsonTask.put(JSON_KEY_TYPE, TASK_TYPE_ARCHIVED_GENERIC);
-				
-				jsonArrayForStorage.add(jsonTask);
-			}
-		}
-	}
-	
+
 	@SuppressWarnings("unchecked")
 	private void convertDeadlineToStore(ObservableList<DeadlineTask> deadlineTaskList, JSONArray jsonArrayForStorage) {
+
 		if (!deadlineTaskList.isEmpty()) {
 			for (int i = 0; i < deadlineTaskList.size(); i++) {
-				DeadlineTask currDeadlineTask = deadlineTaskList.get(i);
+				DeadlineTask currentDeadlineTask = deadlineTaskList.get(i);
 				JSONObject jsonTask = new JSONObject();
-				
-				jsonTask.put(JSON_KEY_NAME, currDeadlineTask.getName());
-				jsonTask.put(JSON_KEY_DUEDATE, currDeadlineTask.getReadableDeadline());
-				jsonTask.put(JSON_KEY_WORKLOAD, currDeadlineTask.getWorkload());
-				jsonTask.put(JSON_KEY_ID, currDeadlineTask.getId());
+
+				jsonTask.put(JSON_KEY_NAME, currentDeadlineTask.getName());
+				jsonTask.put(JSON_KEY_DUEDATE, currentDeadlineTask.getReadableDeadline());
+				jsonTask.put(JSON_KEY_WORKLOAD, currentDeadlineTask.getWorkload());
+				jsonTask.put(JSON_KEY_ID, currentDeadlineTask.getId());
 				jsonTask.put(JSON_KEY_TYPE, TASK_TYPE_DEADLINE);
-				
+
 				jsonArrayForStorage.add(jsonTask);
 			}
 		}
+		
 	}
-	
-	@SuppressWarnings("unchecked")
-	private void convertArchivedDeadlineToStore(ObservableList<DeadlineTask> deadlineTaskList, JSONArray jsonArrayForStorage) {
-		if (!deadlineTaskList.isEmpty()) {
-			for (int i = 0; i < deadlineTaskList.size(); i++) {
-				DeadlineTask currDeadlineTask = deadlineTaskList.get(i);
-				JSONObject jsonTask = new JSONObject();
-				
-				jsonTask.put(JSON_KEY_NAME, currDeadlineTask.getName());
-				jsonTask.put(JSON_KEY_DUEDATE, currDeadlineTask.getReadableDeadline());
-				jsonTask.put(JSON_KEY_WORKLOAD, currDeadlineTask.getWorkload());
-				jsonTask.put(JSON_KEY_ID, currDeadlineTask.getId());
-				jsonTask.put(JSON_KEY_TYPE, TASK_TYPE_ARCHIVED_DEADLINE);
-				
-				jsonArrayForStorage.add(jsonTask);
-			}
-		}
-	}
-	
+
 	@SuppressWarnings("unchecked")
 	private void convertTimedToStore(ObservableList<TimedTask> timedTaskList, JSONArray jsonArrayForStorage) {
-		
+
 		if (!timedTaskList.isEmpty()) {
 			for (int i = 0; i < timedTaskList.size(); i++) {
-				TimedTask currTimedTask = timedTaskList.get(i);
+				TimedTask currentTimedTask = timedTaskList.get(i);
 				JSONObject jsonTask = new JSONObject();
-				
-				jsonTask.put(JSON_KEY_NAME, currTimedTask.getName());
-				jsonTask.put(JSON_KEY_START_TIME, currTimedTask.getReadableStartTime());
-				jsonTask.put(JSON_KEY_END_TIME, currTimedTask.getReadableEndTime());
-				jsonTask.put(JSON_KEY_WORKLOAD, currTimedTask.getWorkload());
-				jsonTask.put(JSON_KEY_ID, currTimedTask.getId());
+
+				jsonTask.put(JSON_KEY_NAME, currentTimedTask.getName());
+				jsonTask.put(JSON_KEY_START_TIME, currentTimedTask.getReadableStartTime());
+				jsonTask.put(JSON_KEY_END_TIME, currentTimedTask.getReadableEndTime());
+				jsonTask.put(JSON_KEY_WORKLOAD, currentTimedTask.getWorkload());
+				jsonTask.put(JSON_KEY_ID, currentTimedTask.getId());
 				jsonTask.put(JSON_KEY_TYPE, TASK_TYPE_TIMED);
-				
+
 				jsonArrayForStorage.add(jsonTask);
 			}
 		}
+		
 	}
-	
+
+	@SuppressWarnings("unchecked")
+	private void convertArchivedGenericToStore(ObservableList<GenericTask> taskList, JSONArray jsonArrayForStorage) {
+		
+		if (!taskList.isEmpty()) {
+			for (int i = 0; i < taskList.size(); i++) {
+				GenericTask currentGenericTask = taskList.get(i);
+				JSONObject jsonTask = new JSONObject();
+
+				jsonTask.put(JSON_KEY_NAME, currentGenericTask.getName());
+				jsonTask.put(JSON_KEY_WORKLOAD, currentGenericTask.getWorkload());
+				jsonTask.put(JSON_KEY_ID, currentGenericTask.getId());
+				jsonTask.put(JSON_KEY_TYPE, TASK_TYPE_ARCHIVED_GENERIC);
+
+				jsonArrayForStorage.add(jsonTask);
+			}
+		}
+		
+	}
+
+	@SuppressWarnings("unchecked")
+	private void convertArchivedDeadlineToStore(ObservableList<DeadlineTask> deadlineTaskList, JSONArray jsonArrayForStorage) {
+
+		if (!deadlineTaskList.isEmpty()) {
+			for (int i = 0; i < deadlineTaskList.size(); i++) {
+				DeadlineTask currentDeadlineTask = deadlineTaskList.get(i);
+				JSONObject jsonTask = new JSONObject();
+
+				jsonTask.put(JSON_KEY_NAME, currentDeadlineTask.getName());
+				jsonTask.put(JSON_KEY_DUEDATE, currentDeadlineTask.getReadableDeadline());
+				jsonTask.put(JSON_KEY_WORKLOAD, currentDeadlineTask.getWorkload());
+				jsonTask.put(JSON_KEY_ID, currentDeadlineTask.getId());
+				jsonTask.put(JSON_KEY_TYPE, TASK_TYPE_ARCHIVED_DEADLINE);
+
+				jsonArrayForStorage.add(jsonTask);
+			}
+		}
+		
+	}
+
 	@SuppressWarnings("unchecked")
 	private void convertArchivedTimedToStore(ObservableList<TimedTask> timedTaskList, JSONArray jsonArrayForStorage) {
-		
+
 		if (!timedTaskList.isEmpty()) {
 			for (int i = 0; i < timedTaskList.size(); i++) {
-				TimedTask currTimedTask = timedTaskList.get(i);
+				TimedTask currentTimedTask = timedTaskList.get(i);
 				JSONObject jsonTask = new JSONObject();
-				
-				jsonTask.put(JSON_KEY_NAME, currTimedTask.getName());
-				jsonTask.put(JSON_KEY_START_TIME, currTimedTask.getReadableStartTime());
-				jsonTask.put(JSON_KEY_END_TIME, currTimedTask.getReadableEndTime());
-				jsonTask.put(JSON_KEY_WORKLOAD, currTimedTask.getWorkload());
-				jsonTask.put(JSON_KEY_ID, currTimedTask.getId());
+
+				jsonTask.put(JSON_KEY_NAME, currentTimedTask.getName());
+				jsonTask.put(JSON_KEY_START_TIME, currentTimedTask.getReadableStartTime());
+				jsonTask.put(JSON_KEY_END_TIME, currentTimedTask.getReadableEndTime());
+				jsonTask.put(JSON_KEY_WORKLOAD, currentTimedTask.getWorkload());
+				jsonTask.put(JSON_KEY_ID, currentTimedTask.getId());
 				jsonTask.put(JSON_KEY_TYPE, TASK_TYPE_ARCHIVED_TIMED);
-				
+
 				jsonArrayForStorage.add(jsonTask);
 			}
 		}
+		
 	}
+	
 }
